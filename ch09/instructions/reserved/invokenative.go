3aea9a122a1db67107e0a05d06d0ca09f6ebac71
cuzz <cuzz1234@163.com>
2019-11-08 18:16:39 +0800

    反射

---
+++ ch09/instructions/reserved/invokenative.go
@@ -0,0 +1,28 @@
+package reserved
+
+import (
+	"fmt"
+	"jvmgo/ch09/instructions/base"
+)
+import "jvmgo/ch09/rtda"
+import "jvmgo/ch09/native"
+import _ "jvmgo/ch09/native/java/lang"
+import _ "jvmgo/ch09/native/sun/misc"
+
+// Invoke native method
+type INVOKE_NATIVE struct{ base.NoOperandsInstruction }
+
+func (self *INVOKE_NATIVE) Execute(frame *rtda.Frame) {
+	method := frame.Method()
+	className := method.Class().Name()
+	methodName := method.Name()
+	methodDescriptor := method.Descriptor()
+	fmt.Println("###" + className + "." + methodName + methodDescriptor)
+	nativeMethod := native.FindNativeMethod(className, methodName, methodDescriptor)
+	if nativeMethod == nil {
+		methodInfo := className + "." + methodName + methodDescriptor
+		panic("java.lang.UnsatisfiedLinkError: " + methodInfo)
+	}
+
+	nativeMethod(frame)
+}
